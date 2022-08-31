3df82c6da0267eea6da4ed2fdf70a646ec6bcde4
Joseph White <81199808+faithfc@users.noreply.github.com>
2022-08-31 16:40:55 -0700

    Add Bootstrap and remove default pages

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import Vue from 'vue'
+import { BootstrapVue } from 'bootstrap-vue'
 import { createPinia, PiniaVuePlugin } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './assets/theme.scss'
 
+Vue.use(BootstrapVue)
 Vue.use(PiniaVuePlugin)
 
 new Vue({
