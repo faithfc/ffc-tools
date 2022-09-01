a43795601af39e3eb3e1b3b792d57fb2b0de177c
Joseph White <81199808+faithfc@users.noreply.github.com>
2022-09-01 14:38:42 -0700

    Basic home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <template>
-  <main>
-    <div>Home</div>
-  </main>
+  <b-container>
+    <main class="mt-3">
+      <b-jumbotron
+          header="FFC Tools"
+          lead="A growing collection of tools for FFC"
+      >
+        <b-button variant="primary" to="/texts">Bible Texts</b-button>
+      </b-jumbotron>
+    </main>
+  </b-container>
 </template>
